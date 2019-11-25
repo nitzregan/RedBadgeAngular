052de1d59989d2857becc2f124a6a11de99b267b
forsonem <forsonem@gmail.com>
2019-11-25 16:47:47 -0500

    authserviceComplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
 
-import { MatToolbarModule } from "@angular/material";
+import { 
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule, 
+  MatInputModule 
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+const routes = [{path: 'register', component: RegistrationComponent}, {path: 'login', component: LoginComponent },{ path: '**', component: RegistrationComponent }];
+
+
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+   
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
