e0040f1291f789c734395176e868f00fdb1ca928
jacobnewby <jacobnewby01@gmail.com>
2019-11-26 14:29:40 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,11 +21,9 @@ import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 import { ProfileIndexComponent } from './components/profile/profile-index/profile-index.component';
-//import { ProfileCreateComponent } from './components/profile/profile-create/profile-create.component';
-//import { ProfileDetailComponent } from './components/profile/profile-detail/profile-detail.component';
 import { ProfileEditComponent } from './components/profile/profile-edit/profile-edit.component';
 import { GetProfileComponent } from './components/profile/get-profile/get-profile.component';
-//import { GetProfileByTeamComponent } from './components/profile/get-profile-by-team/get-profile-by-team.component';
+import { GetProfileByTeamComponent } from './components/profile/get-profile-by-team/get-profile-by-team.component';
 
 import { TeamMessagingService } from './services/team-messaging.service';
 import { TeamMessagingIndexComponent } from './components/team-messaging/team-messaging-index/team-messaging-index.component';
@@ -39,6 +37,9 @@ import { TeamDeleteComponent } from './components/team/team-delete/team-delete.c
 import { TeamIndexComponent } from './components/team/team-index/team-index.component';
 import { TeamUpdateComponent } from './components/team/team-update/team-update.component';
 import { TeamUseridComponent } from './components/team/team-userid/team-userid.component';
+import { TeamMessagingDeleteComponent } from './components/team-messaging/team-messaging-delete/team-messaging-delete.component';
+import { TeamAddplayerComponent } from './components/team/team-addplayer/team-addplayer.component';
+import { TeamRemoveplayerComponent } from './components/team/team-removeplayer/team-removeplayer.component';
 
 
 import { EventIndexComponent } from './components/event/event-index/event-index.component';
@@ -47,23 +48,18 @@ import { AuthService } from './services/auth.service';
 import { EventService } from './services/event.service';
 import { ProfileService } from 'src/app/services/profile.service';
 import { TeamService } from './services/team-services.service';
-import { TeamMessagingDeleteComponent } from './components/team-messaging/team-messaging-delete/team-messaging-delete.component';
-import { TeamAddplayerComponent } from './components/team/team-addplayer/team-addplayer.component';
-import { TeamRemoveplayerComponent } from './components/team/team-removeplayer/team-removeplayer.component';
 
 const routes = [
   { path: 'register', component: RegistrationComponent },
   {path: 'login', component: LoginComponent },
   { path: '**', component: RegistrationComponent },
-  // {
-  //   pathTeamMessages: 'teammessages', children: [
+
       { path : 'teammessages', component: TeamMessagingIndexComponent },
       { path: 'teammessages/create', component: TeamMessagingCreateComponent},
       { path: 'teammessages/detail/:MessageID', component: TeamMessagingDetailComponent },
       { path: 'teammessages/update/:MessageID', component: TeamMessagingUpdateComponent },
       { path: 'teammessages/delete/:MessageID', component: TeamMessagingDeleteComponent},
-  //   ],
-    // pathTeam: 'team', childrenTeam: [
+ 
       { path: 'team', component: TeamIndexComponent },
       { path: 'team/create/:ProfileID', component: TeamCreateComponent },
       { path: 'team/deatail/:TeamID', component: TeamDetailComponent },
@@ -72,19 +68,13 @@ const routes = [
       { path: 'team/userid/:UserID', component: TeamUseridComponent},
       { path: 'team/addplayer/:ProfileID', component: TeamAddplayerComponent},
       { path: 'team/removeplayer/:ProfileID', component: TeamRemoveplayerComponent},
-    // ],
-    // pathEvent: 'event', childrenEvent: [
-      { path: 'event', component: EventIndexComponent },
-    // ],
-    // pathProfile: 'profile', childrenProfile: [
-      { path: 'profile/:TeamID', component: ProfileIndexComponent},
-    //   //{ path: 'profile', component: ProfileCreateComponent},
-    //   //{ path: 'profile', component: ProfileDetailComponent},
-      { path: 'profile', component: ProfileEditComponent},
-    //   //{ path: 'profile', component: GretProfileByTeamComponent},
-      { path: 'profile/:UserID', component: GetProfileComponent},
 
-    // ]
+      { path: 'event', component: EventIndexComponent },
+    
+      { path: 'profile/:TeamID', component: ProfileIndexComponent},
+      { path: 'profile', component: ProfileEditComponent},
+      { path: 'profile/:TeamID', component: GetProfileByTeamComponent},
+      { path: 'profile/:UserID', component: GetProfileComponent},
 ];
 
 
@@ -108,13 +98,11 @@ const routes = [
     LoginComponent,
     EventIndexComponent,
     ProfileIndexComponent,
-    //ProfileCreateComponent,
-    //ProfileDetailComponent,
     ProfileEditComponent,
     GetProfileComponent,
     TeamAddplayerComponent,
     TeamRemoveplayerComponent,
-    //GetProfileByTeamComponent,
+    GetProfileByTeamComponent,
   ],
   imports: [
     BrowserModule,
@@ -130,12 +118,6 @@ const routes = [
     MatInputModule,
     ReactiveFormsModule,
     MatSelectModule,
-    // ProfileIndexComponent,
-    // ProfileCreateComponent,
-    // ProfileDetailComponent,
-    // ProfileEditComponent,
-    // GetProfileComponent,
-    // GetProfileByTeamComponent,
   ],
   
   
